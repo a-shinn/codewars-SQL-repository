d5fac369473255621b1bff70c17a7f147d7a1742
Austin Shinn <81658373+austin-shinn@users.noreply.github.com>
2023-06-16 16:42:45 -0500

    Update exercise_2.sql

--- exercise_2.sql
+++ exercise_2.sql
@@ -1,3 +1,9 @@
+/* CHALLENGE: SQL Bug Fixing: Fix the QUERY - Totaling
+https://www.codewars.com/kata/582cba7d3be8ce3a8300007c
+
+SOLUTION CODE:
+*/
+
 SELECT 
   CAST(s.transaction_date AS date) AS day,
   d.name AS department,
