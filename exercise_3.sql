df03be4f21bd2aa0632a67474bb833a3d527cad4
Austin Shinn <81658373+austin-shinn@users.noreply.github.com>
2023-06-20 14:19:47 -0500

    Update exercise_3.sql

--- exercise_3.sql
+++ exercise_3.sql
@@ -1,1 +1,14 @@
+/* CHALLENGE: SQL Basics: Simple PIVOTING data
+https://www.codewars.com/kata/58126aa90ea99769e7000119
 
+SOLUTION CODE:
+*/
+
+CREATE EXTENSION tablefunc;
+
+SELECT * FROM crosstab(
+    'SELECT name, detail, count(detail) FROM products
+    JOIN details ON products.id = details.product_id
+    GROUP BY name, detail
+    ORDER BY name, detail'
+) AS ct (name text, bad bigint, good bigint, ok bigint)
