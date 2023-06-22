f8622bc4cc0d80bca7a8d149f48d264e72925233
Austin Shinn <81658373+a-shinn@users.noreply.github.com>
2023-06-22 15:47:01 -0500

    Update exercise 2: Fix the QUERY - Totaling.sql

--- exercise 2: Fix the QUERY - Totaling.sql
+++ exercise 2: Fix the QUERY - Totaling.sql
@@ -9,6 +9,6 @@ SELECT
   d.name AS department,
   COUNT(s.id) AS sale_count
   FROM department d
-    INNER JOIN sale s ON d.id = s.department_id
-  GROUP BY day, department
+INNER JOIN sale s ON d.id = s.department_id
+GROUP BY day, department
 ORDER BY day ASC, department ASC;
